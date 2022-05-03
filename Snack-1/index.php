f76e16e940703389a0b61a94fe81442b956fd2a0
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-03 15:53:47 +0200

    Snack-1 done without style

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -4,3 +4,59 @@ punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
  
+<?php
+
+$basket_match = [
+    [
+        'home' => 'Olimpia Milano',
+        'away' => 'Cantù',
+        'home_pt' => 55,
+        'away_pt' => 60
+    ],
+    [
+        'home' => 'Brescia',
+        'away' => 'Varese',
+        'home_pt' => 102,
+        'away_pt' => 71
+    ],
+    [
+        'home' => 'Derthona',
+        'away' => 'Trieste',
+        'home_pt' => 80,
+        'away_pt' => 74
+    ],
+    [
+        'home' => 'Universo Treviso',
+        'away' => 'Virtus Bologna',
+        'home_pt' => 76,
+        'away_pt' => 113
+    ],
+    [
+        'home' => 'Dinamo Sassari',
+        'away' => 'Cremona',
+        'home_pt' => 98,
+        'away_pt' => 82
+    ],
+    [
+        'home' => 'Trento',
+        'away' => 'reggiana',
+        'home_pt' => 69,
+        'away_pt' => 77
+    ],
+    
+]
+
+?>
+
+<h2>
+    <?php
+
+for ($i = 0; $i < count($basket_match); $i++) {
+    $single_match= $basket_match[$i];
+    echo('<pre>');
+    echo('<h3>' . $single_match['home'] . ' - ' . $single_match['away'] . ' | ' . $single_match['home_pt'] . ' - ' . $single_match['away_pt'] . '</h3>');
+    echo('</pre>');
+
+}
+    ?>
+</h2>
