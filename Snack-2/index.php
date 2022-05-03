a52e59b6de3c57cafdc661c28c526b7e7f3b8189
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-03 16:42:56 +0200

    Snack-2 done without style

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -4,3 +4,34 @@ che name sia più lungo di 3 caratteri, che mail contenga un punto
 e una chiocciola e che age sia un numero.
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
+<?php
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age']
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+<body>
+    <div>
+        <h2>
+            <?php
+            if(strlen($name) <= 3 && (!strpos($mail, '.') && !strpos($mail, '@')) && !is_numeric($age)) {
+            echo ('Acccesso negato');
+            } else  {
+            echo ('Accesso eseguito');
+            }
+            ?>
+        </h2>
+    </div>
+</body>
+</html>
